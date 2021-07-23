f9c0f4dbea848f8db45de8a339153a9a62569f71
Stephen Wuh <swuh87@gmail.com>
2021-07-23 09:55:19 -0400

    module 2

--- src/app.js
+++ src/app.js
@@ -9,11 +9,34 @@ app.use(express.static(path.join(__dirname, './public')));
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
 
+const accountData = fs.readFileSync('./json/accounts.json', 'UTF8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('./json/users.json', 'UTF8');
+const users = JSON.parse(userData);
+
 app.get('/', (req,res)=>{
     res.render('index', {
-        title: 'Index',
+        title: 'Account Summary',
+        accounts: accounts
     })
 })
+
+app.get('/savings', (req,res)=>{
+    res.render('account', {account: accounts.savings})
+})
+app.get('/checking', (req,res)=>{
+    res.render('account', {account: accounts.checking})
+})
+app.get('/credit', (req,res)=>{
+    res.render('account', {account: accounts.credit})
+})
+app.get('/profile', (req,res)=>{
+    res.render('profile', {
+        user: users[0]
+    })
+})
+
 
 app.listen(3000, ()=>{
     console.log('PS Project Running on port 3000!');
